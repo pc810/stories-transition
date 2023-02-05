5b3f94d79061b0fbe14cc7c2a6abe8c6ce825752
Priyank Kumar Chaudhari <priyank810123@gmail.com>
2023-02-05 13:49:28 +0530

    asset prefix

--- _next/static/wBdGaiQucl346xJAAI4BS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/939-b4ad5d919d7f8187.js","static/css/c48bcb5b7b165753.css","static/chunks/pages/index-3b5a2e2f3a8ce22a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
